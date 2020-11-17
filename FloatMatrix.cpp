ca83a88fe04ef7c5f35b6658deab9288121dd96d
diogenbip <73134446+diogenbip@users.noreply.github.com>
2020-11-17 20:29:31 +0300

    Add files via upload

--- FloatMatrix.cpp
+++ FloatMatrix.cpp
@@ -6,8 +6,10 @@ using namespace std;
 
 class FloatMatrix
 {
+protected:
     int count_columns;
     int count_strings;
+
     float **matrix;
 public:
 
@@ -74,6 +76,7 @@ public:
         }
         return c;
     }
+   
     void operator=(const FloatMatrix& M)
     {
         count_columns = M.count_columns;
@@ -175,18 +178,78 @@ void FloatMatrix::PowM(const int n) {
     
 }
 
+class SquareMatrix : public FloatMatrix {
+    int size;
+public:
+
+    SquareMatrix(const int _size = 0)
+    {
+        size = _size;
+        count_columns = size;
+        count_strings = size;
+        matrix = new float* [size];
+        for (int i = 0; i < size; i++)
+            matrix[i] = new float[size];
+    }
+    int GetSize()
+    {
+        return size;
+    }
+    float GetMinor(int i, int j)
+    {
+        SquareMatrix b(size - 1);
+        int di=0;
+        int dj = 0;
+        for (int ki = 0; ki < size; ki++) {
+            if (ki == i)
+                di = 1;
+            dj = 0;
+            for (int kj = 0; kj < size - 1; kj++) {
+                if (kj == j)dj = 1;
+                b.matrix[ki][kj] = matrix[ki + di][kj + dj];
+            }
+        }
+        return b.det();
+
+
+    }
+    float det()
+    {
+        float d = 0;
+        
+        int k = 1;
+        if (size == 1)
+        { return matrix[0][0];}
+        else if (size == 2)
+        {
+            return  matrix[0][0] * matrix[1][1] -(matrix[1][0] * matrix[0][1]);
+        }
+        else if (size > 2)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                d = d + k * matrix[i][0] * this->GetMinor(i,0);
+                k = -k;
+            }
+            return d;
+        }
+            
+
+    }
+};
+
 int main()
 {
-    FloatMatrix A(2,2), B(3,2),C(2,2);
+    //FloatMatrix A(2,2), B(3,2),C(2,2);
 
-    A.RandM();
-    B.RandM();
-    cout<<A<<endl;
-    cout << B << endl;
+    SquareMatrix K(3);
+    cin >> K;
+    cout << K.det();
+    /*cout << B << endl;
 
     A = A * B;
     cout << A << endl;
-    A.PowM(2);
+    A.PowM(2);*/
 
   //  if (A != C)
    //     cout << "a!=c" << endl;
